dfc6585beed3831c61216e7c10bb35f5ed7e13b4
hvnovitas-ux <hvnovitas@gmail.com>
2026-09-18 16:18:17 +0200

    Refactor smooth scroll event listener in script.js

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     console.log("🧡 HV Novitas nieuwe homepage geladen");
+
 
     /*
      * SMOOTH SCROLL
      */
 
     document.querySelectorAll('a[href^="#"]').forEach((link) => {
+
         link.addEventListener("click", (event) => {
-            const target = document.querySelector(
-                link.getAttribute("href")
-            );
+
+            const target =
+                document.querySelector(
+                    link.getAttribute("href")
+                );
+
 
             if (target) {
+
                 event.preventDefault();
+
 
                 target.scrollIntoView({
                     behavior: "smooth"
                 });
+
             }
+
         });
+
     });
+
 });
